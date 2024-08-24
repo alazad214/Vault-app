8b8f2d06a999821a64370944b1f46ddfc05392bd
al azad <alazad214@gmail.com>
2024-08-24 17:19:19 +0600

    some changes

--- lib/presentation/onboading/views/onboading_screen.dart
+++ lib/presentation/onboading/views/onboading_screen.dart
@@ -12,7 +12,6 @@ class OnboadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-
     return Scaffold(
       body: Padding(
         padding:
